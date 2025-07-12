b7873920c66d98d37b1685db867ba428a062a307
RahUlkr23r <153869489+RahUlkr23r@users.noreply.github.com>
2025-07-12 23:39:54 +0530

    Update AuthSlice.ts

--- src/State/AuthSlice.ts
+++ src/State/AuthSlice.ts
@@ -16,6 +16,7 @@ export const sendLoginSignupOtp = createAsyncThunk(
       
       console.log("seller created",response.data)
       } catch (error: any) {
+          comsole.log("seller login err",error)
         return rejectWithValue(error.response?.data || "Error sending OTP");
       }
     }
